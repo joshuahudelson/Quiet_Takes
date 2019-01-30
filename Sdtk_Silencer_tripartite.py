4ca7b9dacd8cb709947f69ff5353187efb710ac2
Joshua Hudelson <joshua.hudelson@gmail.com>
2019-01-30 11:16:47 +0200

    Added RMS average to label list for each section.

--- Sdtk_Silencer_tripartite.py
+++ Sdtk_Silencer_tripartite.py
@@ -44,17 +44,13 @@ class Sdtk_Silencer:
         current_segment = self.raw[0:self.window_width]
         current_seg_square = np.square(current_segment)
 
-        TotalRMS = 0
-        Divisor = 0
-
         for hop in range(int((self.rawlen - self.window_width) / self.hop_size)):
 
             RMS = self.get_rms(current_seg_square)
 
             if (RMS < self.rms_threshold) & (np.amax(current_seg_square) < self.maxamp):
                 if self.in_loop:
-                    TotalRMS += RMS
-                    Divisor += 1
+                    pass
                 else:
                     self.in_loop = True
                     start_point = (hop * self.hop_size)
@@ -62,18 +58,14 @@ class Sdtk_Silencer:
                 if self.in_loop:
                     if ((hop * self.hop_size) - start_point) >= self.min_silence_samples:
                         end_point = ((hop - 1) * self.hop_size)
-                        self.silence_labels.append([start_point, end_point, TotalRMS/Divisor])
+                        self.silence_labels.append([start_point, end_point])
                         start_point = None
                         end_point = None
                         self.in_loop = False
-                        TotalRMS = 0
-                        Divisor = 0
                     else:
                         start_point = None
                         end_point = None
                         self.in_loop = False
-                        TotalRMS = 0
-                        Divisor = 0
                 else:
                     pass
 
@@ -86,14 +78,14 @@ class Sdtk_Silencer:
         if self.in_loop:
             end_point = self.rawlen
             if start_point - end_point >= self.min_silence_samples:
-                self.silence_labels.append([start_point, end_point, TotalRMS/Divisor])
+                self.silence_labels.append([start_point, end_point])
 
         return(self.silence_labels)
 
     def export_text_file(self):
         textfile = open(self.filename[:-4] + "_Labels.txt", "w")
         for entry in self.silence_labels:
-            textfile.write(str(entry[0]/float(44100)) + "\t" + str(entry[1]/float(44100)) + "\t" + str(entry[2]) + "\n")
+            textfile.write(str(entry[0]/float(44100)) + "\t" + str(entry[1]/float(44100)) + "\n")
         textfile.close()
 
 if __name__== '__main__':
